1ec363939cca7e75fdade29d12f8df68d1a14ea1
Kishore S <30261988+kishoreio@users.noreply.github.com>
2020-05-12 00:30:59 +0530

    Updates

---
+++ precache-manifest.407b6b5a2aa2582066cd44a20e44327f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8792a09e9304ae124e273eb9e007c1",
+    "url": "/shortify/index.html"
+  },
+  {
+    "revision": "d25dd3448830caee611c",
+    "url": "/shortify/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5be67cf31ecc60c74c35",
+    "url": "/shortify/static/css/main.9d3f8a5b.chunk.css"
+  },
+  {
+    "revision": "d25dd3448830caee611c",
+    "url": "/shortify/static/js/2.e535691b.chunk.js"
+  },
+  {
+    "revision": "3b0760bece22b550433845f448e395fa",
+    "url": "/shortify/static/js/2.e535691b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be67cf31ecc60c74c35",
+    "url": "/shortify/static/js/main.d386ecfc.chunk.js"
+  },
+  {
+    "revision": "091f962894759796a85a",
+    "url": "/shortify/static/js/runtime-main.cf514338.js"
+  },
+  {
+    "revision": "b160bcdd0073ee4a13bb0aa654d34469",
+    "url": "/shortify/static/media/intro.b160bcdd.png"
+  },
+  {
+    "revision": "3a67105488e4231544e3d4f24f43b36a",
+    "url": "/shortify/static/media/login.3a671054.png"
+  },
+  {
+    "revision": "37b2565e544830b7cfe70c14d412766f",
+    "url": "/shortify/static/media/register.37b2565e.png"
+  }
+]);
